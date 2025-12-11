58184958df8b3eb7b80b8fcdaf8120dad2e52a79
Lukas <lukas.nadenau@inform-software.com>
2025-12-11 09:11:26 +0100

    feat: vitest

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()]
+  plugins: [react(),  tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+  },
 })
